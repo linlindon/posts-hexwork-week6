049ebd8da764031f5041e49c8c6f61a67a010ce4
yiling <yiling@25sprout.com>
2024-05-12 19:45:37 +0800

    adjust fields in posts and users model for meta wall

--- models/posts.js
+++ models/posts.js
@@ -4,11 +4,12 @@ const postsSchema = new mongoose.Schema(
 	{
 		title: { type: String, required: [true, '文章標題未填寫'] },
 		content: { type: String, required: [true, '文章內容未填寫'] },
-		author: String,
+		author: { type: String, required: [true, '文章作者未填寫'] },
 		likes: { type: Number, default: 0 },
 		isShown: { type: Boolean, default: true },
 		createdAt: { type: Date, default: Date.now, select: false },
 		tags: [{ type: String, required: [true, '文章標籤未填寫'] }],
+		photo: String,
 	},
 	{
 		versionKey: false
--- models/user.js
+++ models/user.js
@@ -3,11 +3,18 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
 	{
 		name: { type: String, required: [true, '使用者名稱未填寫'] },
-		email: { type: String, required: [true, '使用者 email 未填寫'] },
-		password: { type: String, required: [true, '使用者密碼未填寫'] },
-		phone: String,
+		email: { 
+			type: String, 
+			required: [true, '使用者 email 未填寫'],
+			unique: true, //唯一性
+		},
+		password: { 
+			type: String, 
+			required: [true, '使用者密碼未填寫'],
+			select: false, //不回傳密碼
+		},
 		createdAt: { type: Date, default: Date.now },
-		intro: String,
+		photo: String,
 	},
 	{
 		versionKey: false
